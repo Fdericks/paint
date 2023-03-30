348245bf25316e2abfe3539d4b8cad9657dea8a9
BBrigadoi18 <128743596+BBrigadoi18@users.noreply.github.com>
2023-03-30 12:45:05 -0400

    Add files via upload

--- Triangle.java
+++ Triangle.java
@@ -11,7 +11,10 @@ public class Triangle extends Shape{
 
 	@Override
 	public void draw(Graphics g) {
-		
+		int [] x = {getX() + (getWidth())/2,getX() + getWidth(),getX()}; // the three points of the triangle in (x,y)
+		int [] y = {getY() + -getHeight(),getY(),getY()};
+		// height needs to be negative b/c upside down coordinate plane
+		g.fillPolygon(x, y, 3);
 	}
 	
 	
